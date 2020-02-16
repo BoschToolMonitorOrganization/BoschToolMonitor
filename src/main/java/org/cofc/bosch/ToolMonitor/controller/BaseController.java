2374bfc6a64175876ba393fc5363993bc5b00e57
ryanbarrett1515 <barrettra@g.cofc.edu>
2020-02-16 14:37:10 -0500

    integrates spring with database for files

--- src/main/java/org/cofc/bosch/ToolMonitor/controller/BaseController.java
+++ src/main/java/org/cofc/bosch/ToolMonitor/controller/BaseController.java
@@ -7,14 +7,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.sql.*;
 
 @Controller
 public class BaseController {
-
-    private static String upload_dir = "F:/springfileupload/";
 
     @RequestMapping("/welcome")
     public ModelAndView firstPage() {
@@ -57,10 +56,67 @@ public class BaseController {
                     + carrier.getDownTimeImpact() + "\");");
             stmt.executeBatch();
             stmt.close();
+
+            String sql = "INSERT INTO WPCFiles (workPieceCarrierNumber, valueStream, productionLine, productType, fileName, fileType, data) values (?, ?, ?, ?, ?, ?, ?)";
+            PreparedStatement statement = con.prepareStatement(sql);
+            statement.setString(1, carrier.getWorkPieceCarrierNumber());
+            statement.setString(2, carrier.getValueStream());
+            statement.setString(3, carrier.getProductionLine());
+            statement.setString(4, carrier.getProductType());
+            statement.setString(5, carrier.getJPEG().getOriginalFilename());
+            statement.setString(6, "jpeg");
+            statement.setBlob(7, carrier.getJPEG().getInputStream());
+
+            for(MultipartFile file : carrier.getComponentsList()) {
+                statement.addBatch();
+                statement.setString(1, carrier.getWorkPieceCarrierNumber());
+                statement.setString(2, carrier.getValueStream());
+                statement.setString(3, carrier.getProductionLine());
+                statement.setString(4, carrier.getProductType());
+                statement.setString(5, file.getOriginalFilename());
+                statement.setString(6, "component");
+                statement.setBlob(7, file.getInputStream());
+            }
+
+            for(MultipartFile file : carrier.getComponentsList()) {
+                statement.addBatch();
+                statement.setString(1, carrier.getWorkPieceCarrierNumber());
+                statement.setString(2, carrier.getValueStream());
+                statement.setString(3, carrier.getProductionLine());
+                statement.setString(4, carrier.getProductType());
+                statement.setString(5, file.getOriginalFilename());
+                statement.setString(6, "drawing");
+                statement.setBlob(7, file.getInputStream());
+            }
+
+            for(MultipartFile file : carrier.getPDFs()) {
+                statement.addBatch();
+                statement.setString(1, carrier.getWorkPieceCarrierNumber());
+                statement.setString(2, carrier.getValueStream());
+                statement.setString(3, carrier.getProductionLine());
+                statement.setString(4, carrier.getProductType());
+                statement.setString(5, file.getOriginalFilename());
+                statement.setString(6, "pdf");
+                statement.setBlob(7, file.getInputStream());
+            }
+
+            for(MultipartFile file : carrier.getCADs()) {
+                statement.addBatch();
+                statement.setString(1, carrier.getWorkPieceCarrierNumber());
+                statement.setString(2, carrier.getValueStream());
+                statement.setString(3, carrier.getProductionLine());
+                statement.setString(4, carrier.getProductType());
+                statement.setString(5, file.getOriginalFilename());
+                statement.setString(6, "cad");
+                statement.setBlob(7, file.getInputStream());
+            }
+
+            statement.executeBatch();
+            statement.close();
         } catch (SQLException sqlExc) {
             sqlExc.printStackTrace();
-            System.out.println("Failed to connect to: " + host + "\n" + sqlExc.getMessage());
-        } catch(ClassNotFoundException classNotFound) {
+            System.out.println("Sql exception: " + "\n" + sqlExc.getMessage() + "\n");
+        } catch (ClassNotFoundException classNotFound) {
             classNotFound.printStackTrace();
             System.out.println("class not found exception occured\n" + classNotFound.getMessage());
         } catch (Exception e) {
