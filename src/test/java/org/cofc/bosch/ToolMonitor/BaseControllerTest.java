8c76f42143f5fff236bdca581caee8572bc7bb48
ryanbarrett1515 <barrettra@g.cofc.edu>
2020-02-21 16:00:25 -0500

    changes to tests

--- src/test/java/org/cofc/bosch/ToolMonitor/BaseControllerTest.java
+++ src/test/java/org/cofc/bosch/ToolMonitor/BaseControllerTest.java
@@ -1,34 +1,40 @@
 package org.cofc.bosch.ToolMonitor;
 
+import org.cofc.bosch.ToolMonitor.components.WorkPieceCarrier;
 import org.cofc.bosch.ToolMonitor.controller.BaseController;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
-@WebMvcTest(controllers = BaseController.class)
+@ContextConfiguration(classes = WorkPieceCarrier.class)
 public class BaseControllerTest {
 
 
-    //@Autowired
-    protected MockMvc mockMvc;
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
 
     @Before
-    public void setUp() {
-        mockMvc = MockMvcBuilders.standaloneSetup(BaseController.class).build();
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
     }
 
-
-    // Get request with Param
     @Test
     public void welcome() throws Exception {
         mockMvc.perform(get("/welcome"))
@@ -47,5 +53,15 @@ public class BaseControllerTest {
                 .andExpect(content().string("Repair Ticket Details"));*/
     }
 
+    @Test
+    public void workPieceCarrierSubmission() throws Exception {
+        try {
+            mockMvc.perform(post("/workpiece", new WorkPieceCarrier()))
+                    .andExpect(status().isOk())
+                    .andExpect(view().name("/workPieceCarrierSubmission"))
+                    .andExpect(content().string("Congrats!"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
-
