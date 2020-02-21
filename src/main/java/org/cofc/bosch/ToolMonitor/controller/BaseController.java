482c55531e24a3dc804e647f730ffb0104c89d32
ryanbarrett1515 <barrettra@g.cofc.edu>
2020-02-21 18:15:46 -0500

    fixes tests

--- src/main/java/org/cofc/bosch/ToolMonitor/controller/BaseController.java
+++ src/main/java/org/cofc/bosch/ToolMonitor/controller/BaseController.java
@@ -6,12 +6,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCallback;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
 import java.sql.PreparedStatement;
@@ -25,8 +21,8 @@ public class BaseController {
     private JdbcTemplate jdbcTemplate;
 
     @RequestMapping("/welcome")
-    public ModelAndView firstPage() {
-        return new ModelAndView("welcome");
+    public @ResponseBody String firstPage() {
+        return "welcome";
     }
 
     @GetMapping("/workpiece")
