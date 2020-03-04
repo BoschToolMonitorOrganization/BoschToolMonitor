de20613477ba1076e8b64db31f02797d8698a2a2
ryanbarrett1515 <barrettra@g.cofc.edu>
2020-03-04 10:41:27 -0500

    creates wpc combos and admin page/view page

---
+++ src/main/java/org/cofc/bosch/ToolMonitor/WPCCombo/WPCComboRowMapper.java
@@ -0,0 +1,21 @@
+package org.cofc.bosch.ToolMonitor.WPCCombo;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class WPCComboRowMapper implements RowMapper<WPCCombo> {
+
+    @Override
+    public WPCCombo mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+        WPCCombo combo = new WPCCombo();
+
+        combo.setValueStream(rs.getString("valueStream"));
+        combo.setProductionLine(rs.getString("productionLine"));
+        combo.setProductType(rs.getString("productType"));
+
+        return combo;
+    }
+}
